cc5af77af157dc10c9354ba911e51f8ad4eb8c33
Bhaskar Naidu <naidu.bhaskar1@gmail.com>
2018-03-06 20:32:01 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,11 @@
+mail = "bhaskar@tcs@hello@gmail"
+x = mail.split('@')
+print x
+count = len(x)
+list_2 = []
+for each in x:
+    count = count - 1
+    if count !=0:
+       list.append('@')
+    else:
+       print list_2
